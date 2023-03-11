248b0d0df4bd31b01e7392da80b3bf08c0206aa7
Leonardo Rojas <contacto.leonardo1@gmail.com>
2023-03-11 19:40:00 -0300

    Restructuracióon de archivos

--- jenkins/enviroment/dev/main.tf
+++
@@ -1,8 +0,0 @@
-module "jenkins" {
-  source = "./modules/jenkins"
-
-ami_id = "ami-1234567890"
-instance_type = "t2.micro"
-key_name = "my-ec2-keypair"
-subnet_id = "subnet-1234567890"
-}
--- jenkins/enviroment/dev/variables.tfvars
+++
@@ -1,3 +0,0 @@
-ami_id = "ami-0c94855ba95c71c99"
-instance_type = "t2.micro"
-instance_name = "dev-instance-jenkins"
