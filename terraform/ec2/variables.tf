7af530332fcf7f5e3be78e66e5133fd0bb4de237
EstebanMengoni <emengoni@frba.utn.edu.ar>
2023-03-10 22:38:18 -0300

    Se agrego el modulo de EC2

---
+++ terraform/ec2/variables.tf
@@ -0,0 +1,12 @@
+variable "environment" {
+    description = "The name of the environment"
+    type = string
+}
+variable "ec2_name" {
+    description = "The name of the ec2 instance"
+    type = string
+}
+variable "key_name" {
+    description = "The key name"
+    type = string
+}
