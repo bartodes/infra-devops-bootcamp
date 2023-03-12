17cc384c36f023105234ea6ea48d3681952c634d
bartodes <bartojdes@gmail.com>
2023-03-12 18:09:26 -0300

    changed: commented variables

--- terraform/ec2/variables.tf
+++ terraform/ec2/variables.tf
@@ -1,12 +1,29 @@
 variable "environment" {
-    description = "The name of the environment"
-    type = string
+    description = "The name of the environment."
+    type        = string
 }
+
 variable "ec2_name" {
-    description = "The name of the ec2 instance"
-    type = string
+    description = "The name of the ec2 instance."
+    type        = string
 }
+
 variable "key_name" {
-    description = "The key name"
-    type = string
+    description = "The name of a key pair."
+    type        = string
 }
+
+variable "instance_type" {
+    description = "The instance type of ec2 server."
+    type        = string
+}
+
+variable "private_subnets" {
+    description = "The id of the private subnets."
+    type        = string
+}
+
+# variable "vpc_sg" {
+#     description = "The ID of a security group."
+#     type        = string #Capaz es list(string) 
+# }
