1fa5ec1b99738d3ddd40eccb49775fdca8a49229
bartodes <bartojdes@gmail.com>
2023-03-12 18:03:19 -0300

    feat: change variables and module names

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,15 @@
-module "vpc" {
+module "vpc_root" {
     source = "./network"
 
     environment = var.root_environment
     vpc_name = var.root_vpc_name
 }
 
-module "ec2" {
+module "ec2_instance_root" {
     source = "./ec2"
 
     #                 Outputs from a module
-    private_subnets = module.vpc.private_subnets_id[0]      
+    private_subnets = module.vpc_main.private_subnets_id[0]      
     #vpc_sg          = module # id output from security group module     
 
     #                 Variable in the variables.tf
