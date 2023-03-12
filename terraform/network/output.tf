495443a537d6b7b6d5f9769fdc5a3bb6a93f0e26
bartodes <bartojdes@gmail.com>
2023-03-12 18:06:36 -0300

    commented output sg block

--- terraform/network/output.tf
+++ terraform/network/output.tf
@@ -5,3 +5,7 @@ output "vpc_id" {
 output "private_subnets_id" {
     value = module.vpc.private_subnets
 }
+
+# output "ec2_security_group_id" {
+#     value = module #aca va el id del security group que se cree para la ec2
+# }
