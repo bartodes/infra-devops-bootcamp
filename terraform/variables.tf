d7067558ee291a5e09f262b589b444cf594965b7
bartodes <bartojdes@gmail.com>
2023-03-12 14:50:14 -0300

    Added variables.tf as root file

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "root_environment" {
+    description = "The name of the environment."
+    type        = string
+}
+
+variable "root_vpc_name" {
+    description = "The name of the vpc."
+}
+
+variable "root_ec2_name" {
+    description = "The name of the ec2 server."
+    type        = string 
+}
+
+variable "root_ec2_intsance_type" {
+    description = "The instance type of the ec2 server."
+    type        = string
+}
+
+variable "root_key_name" {
+    description = "The name of a key pair."
+    type        = string
+}
