e78d1f0e0b8929376a37c572c39b9281371fc544
EstebanMengoni <103546076+EstebanMengoni@users.noreply.github.com>
2023-03-13 18:20:22 -0300

    Update providers.tf

--- terraform/eks/providers.tf
+++ terraform/eks/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    access_key                  = "AKIAR4DSWESQ5EDRVM65"
-    secret_key                  = "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"
+    access_key                  = ""
+    secret_key                  = ""
     region                      = "sa-east-1"
 }
