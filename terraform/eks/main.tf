53ed2a3c11dc884f613bc4eadf4c07d0c57b8bae
EstebanMengoni <103546076+EstebanMengoni@users.noreply.github.com>
2023-03-13 21:14:28 +0000

    Modified main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -5,6 +5,7 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
+  cluster_endpoint_private_access  = true 
   cluster_endpoint_public_access  = true
 
   cluster_addons = {
@@ -23,88 +24,46 @@ module "eks" {
   subnet_ids               = var.private_subnets_cluster
   control_plane_subnet_ids = var.private_subnets_control_plane
 
-  # Self Managed Node Group(s)
-  self_managed_node_group_defaults = {
-    instance_type                          = var.node_group_instance_type
-    update_launch_template_default_version = true
-    iam_role_additional_policies = {
-      AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-    }
-  }
-
-  self_managed_node_groups = {
-    one = {
-      name         = "mixed-1"
-      max_size     = 5
-      desired_size = 2
-
-      use_mixed_instances_policy = true
-      mixed_instances_policy = {
-        instances_distribution = {
-          on_demand_base_capacity                  = 0
-          on_demand_percentage_above_base_capacity = 10
-          spot_allocation_strategy                 = "capacity-optimized"
-        }
-
-        override = [
-          {
-            instance_type     = "m5.large"
-            weighted_capacity = "1"
-          },
-          {
-            instance_type     = "m6i.large"
-            weighted_capacity = "2"
-          },
-        ]
-      }
-    }
-  }
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-  }
-
   eks_managed_node_groups = {
     blue = {}
     green = {
       min_size     = 1
-      max_size     = 10
+      max_size     = 3
       desired_size = 1
 
-      instance_types = ["t3.large"]
+      instance_types = var.node_group_instances_type
       capacity_type  = "SPOT"
     }
   }
 
   # aws-auth configmap
-  manage_aws_auth_configmap = true
+#  manage_aws_auth_configmap = true
 
-  aws_auth_roles = [
-    {
-      rolearn  = "arn:aws:iam::66666666666:role/role1"
-      username = "role1"
-      groups   = ["system:masters"]
-    },
-  ]
+#  aws_auth_roles = [
+#    {
+#      rolearn  = "arn:aws:iam::66666666666:role/role1"
+#      username = "role1"
+#      groups   = ["system:masters"]
+#    },
+#  ]
 
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:iam::66666666666:user/user1"
-      username = "user1"
-      groups   = ["system:masters"]
-    },
-    {
-      userarn  = "arn:aws:iam::66666666666:user/user2"
-      username = "user2"
-      groups   = ["system:masters"]
-    },
-  ]
+#  aws_auth_users = [
+#    {
+#      userarn  = "arn:aws:iam::66666666666:user/user1"
+#      username = "user1"
+#      groups   = ["system:masters"]
+#    },
+#    {
+#      userarn  = "arn:aws:iam::66666666666:user/user2"
+#      username = "user2"
+#      groups   = ["system:masters"]
+#    },
+#  ]
 
-  aws_auth_accounts = [
-    "777777777777",
-    "888888888888",
-  ]
+#  aws_auth_accounts = [
+#    "777777777777",
+#    "888888888888",
+#  ]
 
   tags = {
     Environment = var.environment
