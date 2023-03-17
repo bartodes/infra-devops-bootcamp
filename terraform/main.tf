fb03e1fd84aa3a9d5aac02e4ce7b7d7f4d89f2cf
bartodes <bartojdes@gmail.com>
2023-03-16 23:03:35 -0300

    feat: defined user data for ec2 jenkins install

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,15 @@ module "ec2_instance_root" {
 
     ec2_user_data   = <<EOF
     #!/bin/bash
-    echo "Instalacion de Jenkins"
+    curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y jenkins openjdk-11-jre
+    sudo systemctl enable jenkins
+    sudo systemctl start jenkins
     EOF
 }
 
