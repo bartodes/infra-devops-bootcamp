b711fa74384eb086076c0d85574f8c5e0cd6464f
bartodes <bartojdes@gmail.com>
2023-03-16 23:08:03 -0300

    change: moved all files to modules directory

---
+++ terraform/modules/ec2/main.tf
@@ -0,0 +1,21 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = var.ec2_name
+
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  
+  #monitoring            = true Descomemetar cuando se deje de usar localstack como provider
+  vpc_security_group_ids = var.ec2_sg
+  subnet_id              = var.private_subnets
+  user_data              = var.ec2_user_data
+
+  tags = {
+    Terraform   = "true"
+    Name        = var.ec2_name
+    Environment = var.environment
+  }
+}
---
+++ terraform/modules/eks/main.tf
@@ -0,0 +1,71 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  cluster_endpoint_private_access  = true 
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = var.vpc_id
+  subnet_ids               = var.private_subnets_cluster
+  control_plane_subnet_ids = var.private_subnets_control_plane
+
+  eks_managed_node_groups = {
+    green = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+
+      instance_types = ["t2.micro"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # aws-auth configmap
+#  manage_aws_auth_configmap = true
+
+#  aws_auth_roles = [
+#    {
+#      rolearn  = "arn:aws:iam::66666666666:role/role1"
+#      username = "role1"
+#      groups   = ["system:masters"]
+#    },
+#  ]
+
+#  aws_auth_users = [
+#    {
+#      userarn  = "arn:aws:iam::66666666666:user/user1"
+#      username = "user1"
+#      groups   = ["system:masters"]
+#    },
+#    {
+#      userarn  = "arn:aws:iam::66666666666:user/user2"
+#      username = "user2"
+#      groups   = ["system:masters"]
+#    },
+#  ]
+
+#  aws_auth_accounts = [
+#    "777777777777",
+#    "888888888888",
+#  ]
+
+  tags = {
+    Environment = var.environment
+    Terraform   = "true"
+  }
+}
