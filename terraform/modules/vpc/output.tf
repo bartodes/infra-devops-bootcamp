03c3d2522ae484256964f93307261db2dab7099b
EstebanMengoni <103546076+EstebanMengoni@users.noreply.github.com>
2023-03-20 01:10:32 -0300

    Update output.tf

--- terraform/modules/vpc/output.tf
+++ terraform/modules/vpc/output.tf
@@ -2,6 +2,6 @@ output "vpc_id" {
     value = module.vpc.vpc_id
 }
 
-output "private_subnets_id" {
-    value = module.vpc.private_subnets
+output "public_subnets_id" {
+    value = module.vpc.public_subnets
 }
