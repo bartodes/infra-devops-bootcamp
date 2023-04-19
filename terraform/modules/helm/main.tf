bcab1f1ae8524049fd6da78268e163858dcfb685
bartodes <bartojdes@gmail.com>
2023-04-19 13:04:28 -0300

    feat: argocd implementation with helm

---
+++ terraform/modules/helm/main.tf
@@ -0,0 +1,14 @@
+resource "kubernetes_namespace" "argocd" {
+    metadata {
+        name = "argocd"
+    }
+}
+
+resource "helm_release" "argocd" {
+    name       = "argocd-helm"
+    namespace  = kubernetes_namespace.argocd.metadata.0.name
+
+    repository = "https://argoproj.github.io/argo-helm"
+    chart      = "argo-cd"
+    values     = var.values_file
+}
