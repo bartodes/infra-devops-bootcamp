d9c8ffa07a722b55896659c15de9ce790ccd2a0f
bartodes <bartojdes@gmail.com>
2023-04-25 00:19:27 -0300

    feat: iam assumable role open ip connect for argocd implementation

--- terraform/modules/helm/main.tf
+++ terraform/modules/helm/main.tf
@@ -1,3 +1,14 @@
+module "iam_assumable_role_oidc" {
+    source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+    version = "5.17.0"
+
+    create_role                   = true
+    role_name                     = "k8s-argocd-admin"
+    provider_url                  = var.cluster_oidc_provider_url
+    role_policy_arns              = []
+    oidc_fully_qualified_subjects = ["system:serviceaccount:argocd:argocd-server", "system:serviceaccount:argocd:argocd-application-controller"]
+}
+
 resource "kubernetes_namespace" "argocd" {
     metadata {
         name = "argocd"
@@ -9,6 +20,41 @@ resource "helm_release" "argocd" {
     namespace  = kubernetes_namespace.argocd.metadata.0.name
 
     repository = "https://argoproj.github.io/argo-helm"
-    chart      = "argo-cd"
+    chart      = "argocd"
     values     = [var.values_file]
+
+    set {
+        name  = "server.service.type"
+        value = "LoadBalancer"
+    }
+
+    set {
+        name  = "server.configEnabled"
+        value = "true"
+    }
+
+    set {
+        name  = "server.name"
+        value = "argocd-server"
+    }
+
+    set {
+        name  = "server.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+        value = module.iam_assumable_role_oidc.iam_role_arn
+    }
+
+    set {
+        name  = "controller.args.appResyncPeriod"
+        value = "30"
+    }
+
+    set {
+        name  = "controller.args.repoServerTimeoutSeconds"
+        value = "15"
+    }
+
+    depends_on = [
+        kubernetes_namespace.argocd,
+        module.iam_assumable_role_oidc
+    ]
 }
