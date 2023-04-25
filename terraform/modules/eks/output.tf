b813ca2dfc7082c6622e2d4f168e273ac63bc58b
bartodes <bartojdes@gmail.com>
2023-04-25 00:17:57 -0300

    feat: open id connect url output

--- terraform/modules/eks/output.tf
+++ terraform/modules/eks/output.tf
@@ -5,8 +5,15 @@ output "cluster_name" {
 
 output "cluster_ca_certificate" {
     value = module.eks.cluster_certificate_authority_data
+    description = "The CA certificate."
 }
 
 output "cluster_endpoint" {
     value = module.eks.cluster_endpoint
+    description = "The cluster endpoint."
+}
+
+output "cluster_oidc_provider_url" {
+    value = module.eks.oidc_provider
+    description = "The Open ID Connect identity provider."
 }
